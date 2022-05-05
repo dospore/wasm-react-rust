85d716861e5f80bdb676efd384acdfdca865cbb1
Dospore <tetthera@gmail.com>
2022-05-05 16:07:27 +1000

    Commit build

---
+++ build/precache-manifest.a29ed49ec1c4fdc1c66feb1713bf38bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d4c1ffd5fedc6c8409d",
+    "url": "/99634a67140676a964b3.module.wasm"
+  },
+  {
+    "revision": "c6bf475fc786f3f98e3dab3b8f6b968a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f479ac4cee261236427",
+    "url": "/static/css/main.c5349728.chunk.css"
+  },
+  {
+    "revision": "e70b10162db093f236ac",
+    "url": "/static/js/2.fd8f53f8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fd8f53f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d4c1ffd5fedc6c8409d",
+    "url": "/static/js/3.48500831.chunk.js"
+  },
+  {
+    "revision": "3f479ac4cee261236427",
+    "url": "/static/js/main.22a19d29.chunk.js"
+  },
+  {
+    "revision": "be47fc7f3c2d20665ad9",
+    "url": "/static/js/runtime-main.bac1dbf4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
